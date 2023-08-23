5e750b1172e6f48fd8c3acd78587a18261d3450b
arduriki <jordi.ardura.professional@gmail.com>
2023-08-23 12:32:57 +0200

    Chapter 4 methods use instance variables

---
+++ 04_methods_variables/Value.java
@@ -0,0 +1,10 @@
+class Value {
+    int intValue;
+    public int doStuff(int factor) {
+        if (intValue > 100) {
+            return intValue * factor;
+        } else {
+            return intValue * (5 - factor);
+        }
+    }
+}
