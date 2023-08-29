fa597820a80bc6cbcd3085a3eb13004ccb3488df
arduriki <jordi.ardura.professional@gmail.com>
2023-08-29 16:11:25 +0200

    Simple startup game

--- 05_simple_startup_game/SimpleStartup.java
+++ 05_simple_startup_game/SimpleStartup.java
@@ -1,13 +1,15 @@
-class SimpleStartup {
+public class SimpleStartup {
     private int[] locationCells;
     private int numOfHits = 0;
 
-    public void setLocationCells (int[] locs) {
+    public void setLocationCells(int[] locs){
         locationCells = locs;
     }
 
+
     public String checkYourself(int guess) {
         String result = "miss";
+
         for (int cell : locationCells) {
             if (guess == cell) {
                 result = "hit";
@@ -21,4 +23,5 @@ class SimpleStartup {
         System.out.println(result);
         return result;
     }  // end method
-}  // close class
+}
+
