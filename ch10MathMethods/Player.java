7d4987fa3a0a30f783a45f9e4eb237dad042afe1
arduriki <jordi.ardura.professional@gmail.com>
2023-10-25 12:14:27 +0200

    static variables

---
+++ ch10MathMethods/Player.java
@@ -0,0 +1,11 @@
+package ch10MathMethods;
+
+class Player {
+    static int playerCount = 0;
+    private String name;
+    
+    public Player (String n) {
+        name = n;
+        playerCount++;
+    }
+}
