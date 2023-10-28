b746e0c659fc43de1b8c8ce89e304e3cae4106d3
arduriki <jordi.ardura.professional@gmail.com>
2023-10-28 11:14:00 +0200

    static methods

---
+++ ch10NumbersStatistics/Player.java
@@ -0,0 +1,10 @@
+package ch10NumbersStatistics;
+
+public class Player {
+    static int playerCount = 0;
+    private String name;
+    public Player(String n) {
+        name = n;
+        playerCount++;
+    }
+}
